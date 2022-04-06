1cf5f93a3fbac80bf932a93f1384ad5c720c44d4
Heli Xu <xuheli91@gmail.com>
2022-04-05 23:00:12 -0400

    subset advertise from rev_largest

--- 2022-04-05/digital media.R
+++ 2022-04-05/digital media.R
@@ -3,10 +3,13 @@ library(tidyverse)
 tuesdata <- tidytuesdayR::tt_load('2022-04-05')
 news_orgs <- tuesdata$news_orgs
 
-news_orgs %>% 
+ad_rev_budget <- news_orgs %>% 
   group_by(budget_percent_editorial, revenue_stream_largest) %>% 
   count() %>% 
   group_by(budget_percent_editorial) %>% 
   mutate(pct_rev=n/sum(n)*100) %>% 
+  filter(revenue_stream_largest=="Direct sold advertising")
   view()
 
+
+ggplot()
