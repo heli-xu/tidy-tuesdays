8a1e0af539abef253b167806435b2480955aae88
Heli Xu <xuheli91@gmail.com>
2022-04-12 22:39:04 -0400

    drop_na added

--- 2022-04-05/digital media.R
+++ 2022-04-05/digital media.R
@@ -6,6 +6,7 @@ news_orgs <- tuesdata$news_orgs
 ad_rev_budget <- news_orgs %>% 
   group_by(budget_percent_editorial, revenue_stream_largest) %>% 
   count() %>% 
+  drop_na() %>% 
   group_by(budget_percent_editorial) %>% 
   mutate(pct_rev=n/sum(n)*100) %>% 
   filter(revenue_stream_largest=="Direct sold advertising") %>% 
