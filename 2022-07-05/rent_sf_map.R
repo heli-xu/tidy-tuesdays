62a58f23ec42237a7914c9005d90d35c4eab2f9f
Heli Xu <xuheli91@gmail.com>
2022-10-24 17:39:08 -0400

    bay area rent by county, addpolygons (leaflet)__customizing to be added
    shapefile downloaded from ca.gov, join with original coord to generate county info that match; summarise median rent by county, link to polygon shape, then map

--- 2022-07-05/rent_sf_map.R
+++ 2022-07-05/rent_sf_map.R
@@ -1,8 +1,9 @@
 library(tidyverse)
 library(skimr)
 library(ggplot2)
-library(tidygeocoder)
+#library(tidygeocoder)
 library(leaflet)
+library(sf)
 
 # import data
 rent <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-07-05/rent.csv')
@@ -31,11 +32,13 @@ rent_geo_2015 <- rent_geo %>%
 
 skim(rent_geo)
 
+# set color palette for circles
 pal <- colorNumeric(
   palette = c("orange","navy"),
   domain = rent_geo_2015$price
 )
 
+# map for 2015, 1 bed
 leaflet(data = rent_geo_2015 %>% arrange(price)) %>% 
   addProviderTiles(providers$CartoDB.Voyager) %>% 
   #add map markers
@@ -55,7 +58,9 @@ leaflet(data = rent_geo_2015 %>% arrange(price)) %>%
   setView(lng = mean(rent_geo_2015$lon),
           lat = mean(rent_geo_2015$lat),
           zoom = 8)
-             
+
+# creating a function to look at different year/bed  
+# given you have data rent_geo
 map_rent_year_beds <- function(year, beds){
   rent_geo_map <- rent_geo %>% 
     filter(year==year,
@@ -91,12 +96,87 @@ map_rent_year_beds <- function(year, beds){
 map_rent_year_beds(2010, 1)
 
 
-## graveyard code
-leaflet(data = quakes[1:20,]) %>% 
-  addTiles() %>%
-  addMarkers(~long, ~lat, popup = ~as.character(mag), label = ~as.character(mag))
+# get zip code info from shapefile
+unzip("Bay Area Counties.zip",
+      exdir = "bay_county_shapefile", junkpaths = T,
+      overwrite = F)
 
-leaflet(data = slice(rent_geo,1:50)) %>% 
-  addTiles() %>%
-  addCircles(~lon, ~lat)
+bay_county <- st_read("bay_county_shapefile/geo_export_62cf50c7-6bad-453b-a1f1-701b46d43fd2.shp")
+# Geodetic CRS: WGS84 (its EPSG code = 4236)
 
+
+##very important
+##generate a column "geometry" based on lat and lon that can be recognized later on
+rent_geometry <- st_as_sf(rent_geo,
+                         coords = c(x = "lon", y = "lat"),
+                         remove = FALSE, #keep the coords columns for leaflet
+                         crs = st_crs(4326)) ##matching your shp file
+
+# adding zipcode to each point in data (based on "geometry")
+rent_geo_county <- st_join(rent_geometry, bay_county, join = st_within)
+
+#define epsg code if it's not defined
+# st_crs(x)$epsg --> NA in this case
+# st_crs(x)$proj4string  --> "+proj=longlat +ellps=WGS84 +no_defs"
+# CRS may have both but may only need one,
+# for joining, you need matching crs code, so if undefined, need assign manually
+st_crs(bay_county) <- 4326
+#then join (as above)
+
+
+
+skim(rent_geo_county) #mostly looking at the generated zip column
+
+rent_geo_county2 <- rent_geo_county %>% 
+  drop_na(county) %>% 
+  group_by(county, year, beds) %>% 
+  summarise(median = median(price),
+            count = n(),
+            .groups = "drop") %>% 
+  ungroup() %>% 
+  left_join(bay_county, by = "county") %>% 
+  st_as_sf() %>% 
+  filter(year == 2015,
+         beds == 1)
+
+##looking at 2015, 1 bed
+
+pal2 <- colorNumeric(
+  palette = c("orange", "navy"),
+  domain = rent_geo_county2$median
+)
+
+county_label <- paste0(
+  rent_geo_county2$county, "\n", "median rent $", rent_geo_county2$median
+
+)
+
+
+leaflet(rent_geo_county2) %>% 
+  addProviderTiles(providers$CartoDB.Voyager) %>% 
+  addPolygons(color = "white", 
+              weight = 2,
+              smoothFactor = 0.5,
+              opacity = 1,
+              fillColor = ~pal2(median),
+              fillOpacity = 0.8,
+              highlightOptions = highlightOptions(
+                weight = 5,
+                color = "#666",
+                fillOpacity = 0.8,
+                bringToFront = TRUE),
+              label = county_label,
+              labelOptions = labelOptions(
+                style = list(
+                  "font-family" = "Fira Sans, sans-serif",
+                  "font-size" = "1.2em"
+                ))
+              ) %>% 
+  addLegend("bottomleft",
+            pal = pal2,
+            values = ~median,
+            title = "Rent \n(2015, 1 bedroom)",
+            labFormat = labelFormat(prefix = "$"),
+            opacity = 1)
+
+    
